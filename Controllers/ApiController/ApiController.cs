20c666cacbca3fb848e0affc10dbc4f70b09ee40
LuisMoisesOlveraSantiago <luismoises.olverasantiago.gen23@cbtis105.edu.mx>
2025-03-26 08:52:17 -0600

    ya quedo :v

--- Controllers/ApiController/ApiController.cs
+++ Controllers/ApiController/ApiController.cs
@@ -1,16 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 [ApiController]
 [Route("conexion")]
 public class Conexion : Controller {
     [HttpGet("mongo")]
     public IActionResult ListarThe_CursedMongoDB(){
-        MongoClient client = new MongoClient(CadenasConexion.MONGO_DB);
+        MongoClient client = new MongoClient(CadenasConexion.Mongo_DB);
         var db = client.GetDatabase("Practica2_Alejandro_Luis");
-        var collection = db.GetCollection<The_CursedMongo("The_Cursed");
+        var collection = db.GetCollection<TheCursed>("The_Cursed");
 
-        var lista = collection.Find(FilterDefinition<The_CursedMongo>.Empty).ToList();
+        var lista = collection.Find(FilterDefinition<TheCursed>.Empty).ToList();
 
-        return Ok (Lista);
+        return Ok (lista);
 
     }
 }
--- Models/TheCursed.cs
+++ Models/TheCursed.cs
@@ -4,13 +4,14 @@ public class TheCursed {
     [BsonId]
     [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
     public string? Id { get; set; }
-    public int Personaje { get; set; }
-    public int Daño { get; set; }
+    public string Personaje { get; set; } = "";
+    public int Salud { get; set; }  
+    public int Ataque { get; set; }
     public int Velocidad { get; set; }
-    public int Sangrado { get; set; }
-    public string Alimentacion { get; set; }
-    public string Altura { get; set; }
-    public int RopturaHueso { get; set; }
+    public int? Sangrado { get; set; }
+    public string Alimentacion { get; set; } = "";
+    public string Altura { get; set; } = "";
+    public int? RopturaHueso { get; set; }
 
 
 
